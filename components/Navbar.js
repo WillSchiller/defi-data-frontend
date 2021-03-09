71c20bf807f309b39b24fb0492a87b5515e4ca79
chinotto <chinottoserver@protonmail.com>
2021-03-09 18:28:11 +0000

    fix image

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,7 +3,6 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import Image from 'next/image'
 
 
 
@@ -14,7 +13,7 @@ export default function Navbar() {
 
           <div class="navbar">
         <div class= "logo">
-        <Image
+        <img
         src="/defi_data_logo.png"
         alt="defi data logo"
         width={100}
