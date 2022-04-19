c13a80c41a8cb4e6072c143b2f6c46464615746e
Will Schiller <will.schiller@protonmail.com>
2022-04-19 02:21:44 +0100

    update readme

--- next.config.js
+++ next.config.js
@@ -4,4 +4,13 @@ module.exports = {
       locales: ["en"],
       defaultLocale: "en",
     },
+    async redirects() {
+      return [
+        {
+          source: '/[symbol]',
+          destination: '/',
+          permanent: true,
+        },
+      ]
+    },
   };
