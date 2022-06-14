cfdf9f2a660292bb815d588d17bb4847a4cba2f1
Will Schiller <will.schiller@protonmail.com>
2022-06-14 12:27:15 +0100

    re-style

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
           <div className='light-header'>
 
             <Image src="/gitcoin.png" alt="defi data logo" width={620} height={200} />
-           <p className='no-margin'> <Link href="https://gitcoin.co/grants/5860/defidata-free-crypto-twitter-analytics" ><a className='green' target="_blank">FUND DEVS</a></Link> TO <span className='white'>#BUIDL</span> DEFIDATA</p>
+           <p> <Link href="https://gitcoin.co/grants/5860/defidata-free-crypto-twitter-analytics" ><a className='button' target="_blank">FUND DEVS</a></Link> TO <span className='white'>#BUIDL</span> DEFIDATA</p>
         
                 
             
