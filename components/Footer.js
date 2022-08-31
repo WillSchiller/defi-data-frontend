02fc5da68907cff2d218bdff34fb1c35236a0ad9
Will Schiller <will.schiller@protonmail.com>
2022-08-31 02:53:25 +0100

    addr update

--- components/Footer.js
+++ components/Footer.js
@@ -7,7 +7,7 @@ export default function Footer({ title }) {
                 Twitter: <Link href="https://twitter.com/defidatadev" ><a  target="_blank" > @DefiDataDev</a></Link>
             </p>
             <p>
-                Donate ETH: <span className='white'>0xb94d23CE4Aac668015aF33371dA35cBB4dC461df</span>
+                Donate ETH: <span className='white'>0x8ae3784B96DeF82936124AF47E3056699d6F2Cd1</span>
             </p>
         </div>
     )
